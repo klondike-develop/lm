e0570abc5c304a580f622031c7da7752ecdca407
Igor Melnikov <igor2354@yandex.ru>
2022-05-16 13:23:06 +0300

    16

--- js/new-product-collections.js
+++ js/new-product-collections.js
@@ -1,24 +1,24 @@
-$(document).ready(function() {
-    $.each($(".new-collections"), function(index, val) {
-        var slider = $(val).find(".new-collections-slider");
-        var btnPrev = $(val).find(".swiper-button-prev");
-        var btnNext = $(val).find(".swiper-button-next");
+$(document).ready(function () {
+	$.each($(".new-collections"), function (index, val) {
+		var slider = $(val).find(".new-collections-slider");
+		var btnPrev = $(val).find(".swiper-button-prev");
+		var btnNext = $(val).find(".swiper-button-next");
 
-        var newCollectionsSlider = new Swiper(slider[0], {
-            slidesPerView: 3,
-            watchOverflow: true,
-            spaceBetween: 15,
-    
-            navigation: {
-                nextEl: btnNext[0],
-                prevEl: btnPrev[0],
-            },
-    
-            breakpoints: {
-                1326: {
-                    slidesPerView: 5
-                }
-            },
-        });
-    });
+		var newCollectionsSlider = new Swiper(slider[0], {
+			slidesPerView: 3,
+			watchOverflow: true,
+			spaceBetween: 15,
+
+			navigation: {
+				nextEl: btnNext[0],
+				prevEl: btnPrev[0],
+			},
+
+			breakpoints: {
+				1326: {
+					slidesPerView: 5,
+				},
+			},
+		});
+	});
 });
