66033c7309209834b7dc4202e3894e9d89a2f38d
SadCat88 <sadcat88@yandex.ru>
2022-07-12 17:54:30 +0300

    38

---
+++ js/new-product-5-new-similar-products.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+	var newSimilarProductsSlider;
+	var newSimilarProductsSliderLength = $(".new-similar-products-slider-container").length;
+
+	if (newSimilarProductsSliderLength) {
+		newSimilarProductsSlider = new Swiper(".new-similar-products-slider-container", {
+			slidesPerView: 5,
+			watchOverflow: true,
+			spaceBetween: 16,
+			speed: 500,
+
+			navigation: {
+				nextEl: ".new-similar-products-slider .swiper-button-next",
+				prevEl: ".new-similar-products-slider .swiper-button-prev",
+			},
+
+			breakpoints: {
+				1326: {
+					slidesPerView: 5,
+				},
+			},
+
+			on: {
+				unlock(swiper) {
+					this.$el[0].classList.remove("lock");
+				},
+
+				lock(swiper) {
+					this.$el[0].classList.add("lock");
+				},
+			},
+		});
+	}
+});
