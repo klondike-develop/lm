9effadb778cfc8cd32f0f9ad9c0638cb8c2e3cd9
Denis Antukh <klondike-develop@mail.ru>
2022-09-25 12:39:38 +0300

    Создание раздела Сравнение

---
+++ js/comparison.js
@@ -0,0 +1,158 @@
+$(document).ready(function () {
+
+    //SCROLLING
+
+    function scrolling() {
+        $wW = $(window).width();
+        $wH = $(window).height();
+
+        $top1 = $('body').scrollTop();
+        $top2 = $('html').scrollTop();
+
+        if ($top1) {
+            $top = $top1;
+        } else {
+            $top = $top2;
+        }
+
+        $fixedSliderInit = $('.box:visible .fixed-slider-init');
+        $fixedSliderInitTop = $fixedSliderInit.offset().top;
+        $fixedSliderPlaceholder = $('.box:visible .fixed-slider-placeholder');
+        $fixedSliderPlaceholderH = $fixedSliderPlaceholder.height();
+
+        console.log($top, $fixedSliderInitTop);
+
+        if ($top > $fixedSliderInitTop + $fixedSliderPlaceholderH) {
+            $('.box:visible .fixed-slider').addClass('scrolled');
+            $('.box:visible .fixed-slider').css('top', $top - $fixedSliderInitTop);
+        } else {
+            $('.box:visible .fixed-slider').removeClass('scrolled');
+            $('.box:visible .fixed-slider').css('top', 0);
+        }
+    }
+
+    scrolling();
+    $(window).on("scroll", scrolling);
+
+    function sliderJsComparison() {
+
+        $(".options-list").each(function () {
+            $this = $(this);
+            $options = [];
+
+            $this.find(".comparison-option").css('height', 'auto');
+
+            $this.find(".comparison-option").each(function () {
+                $optIndex = $(this).data('option');
+
+                if ($options[$optIndex]) {
+                    if ($(this).height() > $options[$optIndex]) {
+                        $options[$optIndex] = $(this).height();
+                    }
+                } else {
+                    $options[$optIndex] = $(this).height();
+                }
+            });
+
+            $.each($options, function (key, value) {
+                if (value) {
+                    $this.find('[data-option=' + key + ']').height(value);
+                }
+            });
+        });
+
+        $(".slider-js-comparison").each(function () {
+            $th = 0;
+
+            $(this).find(".product-thumb-title").css('height', 'auto');
+            $(this).find(".product-thumb-title").each(function () {
+                if ($(this).height() > $th) {
+                    $th = $(this).height();
+                }
+            });
+            $(this).find(".product-thumb-title").css('height', $th);
+        });
+
+        $(".slider-js-comparison").slick({
+            dots: false,
+            arrows: true,
+            draggable: false,
+            infinite: false,
+            centerMode: false,
+            centerPadding: "0px",
+            autoplay: false,
+            autoplaySpeed: 5000,
+            speed: 500,
+            pauseOnHover: false,
+            pauseOnDotsHover: false,
+            slide: ".slide-js",
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            swipeToSlide: false,
+            responsive: [
+                {
+                    breakpoint: 1280,
+                    settings: {
+                        slidesToShow: 3,
+                    }
+                },
+            ]
+        });
+
+        $(".slider-js-comparison-linked").each(function () {
+            $(this).slick({
+                dots: false,
+                arrows: false,
+                draggable: true,
+                infinite: false,
+                centerMode: false,
+                centerPadding: "0px",
+                autoplay: false,
+                autoplaySpeed: 5000,
+                speed: 500,
+                pauseOnHover: false,
+                pauseOnDotsHover: false,
+                slide: ".slide-js",
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                swipeToSlide: true,
+                responsive: [
+                    {
+                        breakpoint: 1280,
+                        settings: {
+                            slidesToShow: 3,
+                        }
+                    },
+                ]
+            });
+        });
+
+        $('.slider-js-comparison').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+
+            $(this).closest('.box').find(".slider-js-comparison-linked").each(function () {
+                $(this).slick('slickGoTo', nextSlide);
+                console.log(event);
+            });
+        });
+
+        $('.slider-js-comparison').each(function () {
+            $tH = $(this).closest('.fixed-slider').height();
+            $(this).closest('.fixed-slider').next('.fixed-slider-placeholder').height($tH);
+        });
+    }
+
+    sliderJsComparison();
+
+    $('.comparison-page .global-tabs').click(function () {
+        setTimeout(function () {
+            $('.slider-js-comparison, .slider-js-comparison-linked').slick('unslick');
+            sliderJsComparison();
+
+            $('.slider-js-comparison').each(function () {
+                $tH = $(this).closest('.fixed-slider').height();
+                $(this).closest('.fixed-slider').next('.fixed-slider-placeholder').height($tH);
+            });
+        }, 1);
+    });
+
+});//document ready
